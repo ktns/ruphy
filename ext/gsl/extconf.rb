f22eb44e22d6a35ce1227a006991e9e5d543b6df
Katsuhiko Nishimra <kat841@hotmail.com>
2010-04-29 05:55:34 +0900

    extention build dependency check
    for libgsl

--- ext/gsl/extconf.rb
+++ ext/gsl/extconf.rb
@@ -2,4 +2,14 @@ require 'mkmf'
 
 dir_config("gsl")
 
+def gsl_check
+	find_library('gsl', 'gsl_sf_legendre_Plm') or
+	find_library('gslcblas', 'cblas_ctrmv') and
+	find_library('gsl', 'gsl_sf_legendre_Plm')
+end
+
+unless gsl_check
+	raise 'Extention build dependency is not met! Please install libgsl!'
+end
+
 create_makefile("gsl")
