c40ca0ee6caa5b0778d20131f27d85ec9dee23fc
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-01 23:59:19 +0900

    defined class RuPHY::GSL::SPWF

--- ext/gsl/gsl.c
+++ ext/gsl/gsl.c
@@ -2,9 +2,11 @@
 
 static VALUE rb_mRuPHY;
 static VALUE rb_mGSL;
+static VALUE rb_cSPWF;
 
 void Init_gsl()
 {
 	rb_mRuPHY = rb_define_module("RuPHY");
 	rb_mGSL   = rb_define_module_under(rb_mRuPHY, "GSL");
+	rb_cSPWF  = rb_define_class_under(rb_mGSL, "SPWF", rb_cObject);
 }
