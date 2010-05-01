4f5ffc01e67a6f18140312b8ff2f0f0b865a5b5d
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-01 23:58:18 +0900

    spec: class RuPHY::GSL::SPWF should be defined

--- spec/gsl_extn_spec.rb
+++ spec/gsl_extn_spec.rb
@@ -7,4 +7,8 @@ describe "gsl" do
 	it "should define module RuPHY::GSL" do
 		RuPHY::GSL.should be_instance_of(Module)
 	end
+
+	it 'should define class RuPHY::GSL::SPWF' do
+		RuPHY::GSL::SPWF.should be_instance_of(Class)
+	end
 end
