f47f43a1f5b8db042040d4975dacad2c40a12442
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-01 22:02:57 +0900

    indentation

--- lib/ruphy.rb
+++ lib/ruphy.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+$:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Ruphy
-  VERSION = '0.0.1'
+	VERSION = '0.0.1'
 end
