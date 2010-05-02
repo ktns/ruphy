b527bac22343724eca937b33a7e8e0475d0a8cfa
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-03 02:33:23 +0900

    defined class RuPHY::GSL::SPWF::Hydrogenic

--- ext/gsl/spwf.c
+++ ext/gsl/spwf.c
@@ -1,7 +1,10 @@
 #include "gsl.h"
 
 static VALUE rb_cSPWF;
+static VALUE rb_cHydrogenic;
 
 void init_SPWF(void) {
 	rb_cSPWF  = rb_define_class_under(rb_mGSL, "SPWF", rb_cObject);
+
+	rb_cHydrogenic = rb_define_class_under(rb_cSPWF,"Hydrogenic", rb_cSPWF);
 }
