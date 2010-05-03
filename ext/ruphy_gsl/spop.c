7271edb98781286d658e18f570cc4230d040d145
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-04 05:07:11 +0900

    defined class GSL::SPOP::Hamiltonian, ::Hydrogenic

--- ext/ruphy_gsl/spop.c
+++ ext/ruphy_gsl/spop.c
@@ -1,7 +1,11 @@
 #include "ruphy_gsl.h"
 
 static VALUE rb_cSPOP;
+static VALUE rb_cHamiltonian;
+static VALUE rb_cHydrogenic;
 
 void init_SPOP(void) {
-	rb_cSPOP = rb_define_class_under(rb_mGSL, "SPOP", rb_cObject);
+	rb_cSPOP        = rb_define_class_under(rb_mGSL, "SPOP", rb_cObject);
+	rb_cHamiltonian = rb_define_class_under(rb_cSPOP, "Hamiltonian", rb_cSPOP);
+	rb_cHydrogenic  = rb_define_class_under(rb_cHamiltonian, "Hydrogenic", rb_cHamiltonian);
 }
--- spec/ruphy_gsl_extn_spec.rb
+++ spec/ruphy_gsl_extn_spec.rb
@@ -19,10 +19,30 @@ describe "ruphy_gsl" do
 	it 'should define class RuPHY::GSL::SPOP' do
 		RuPHY::GSL::SPOP.should be_instance_of(Class)
 	end
+
+	it 'should define class RuPHY::GSL::SPOP::Hamiltonian' do
+		RuPHY::GSL::SPOP::Hamiltonian.should be_instance_of(Class)
+	end
+
+	it 'should define class RuPHY::GSL::SPOP::Hamiltonian::Hydrogenic' do
+		RuPHY::GSL::SPOP::Hamiltonian::Hydrogenic.should be_instance_of(Class)
+	end
 end
 
 describe RuPHY::GSL::SPWF::Hydrogenic do
 	it 'should be descendant of SPWF' do
 		RuPHY::GSL::SPWF::Hydrogenic.should < RuPHY::GSL::SPWF
+	end
+end
+
+describe RuPHY::GSL::SPOP::Hamiltonian do
+	it 'should be descendant of SPOP' do
+		RuPHY::GSL::SPOP::Hamiltonian.should < RuPHY::GSL::SPOP
+	end
+end
+
+describe RuPHY::GSL::SPOP::Hamiltonian::Hydrogenic do
+	it 'should be descendant of Hamiltonian' do
+		RuPHY::GSL::SPOP::Hamiltonian::Hydrogenic.should < RuPHY::GSL::SPOP::Hamiltonian
 	end
 end
