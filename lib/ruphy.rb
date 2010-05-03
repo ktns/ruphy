403f4c215af50829ec9647a2704e7e3a377289f6
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-03 16:42:17 +0900

    defined RuPHY.complex

--- lib/ruphy.rb
+++ lib/ruphy.rb
@@ -1,8 +1,17 @@
 $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'complex'
+
 module RuPHY
 	VERSION = '0.0.1'
+
+	def self.complex x, y
+		if v = [x,y].find{|v|not v.class <= Numeric}
+			raise TypeError, "Numeric expected, but `#{v.class}'"
+		end
+		Complex(x,y)
+	end
 end
 
 begin
--- spec/ruphy_spec.rb
+++ spec/ruphy_spec.rb
@@ -1,5 +1,20 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+describe RuPHY do
+	describe '.complex' do
+		it 'should return Complex' do
+			RuPHY.should respond_to(:complex)
+			RuPHY.complex(1,0).should be_kind_of(Complex)
+		end
+
+		it 'should fail if non-numeric was given' do
+			lambda do
+				RuPHY.complex('1',0)
+			end.should raise_error TypeError
+		end
+	end
+end
+
 describe RuPHY::GSL::SPWF do 
 	describe '.new' do
 		it 'should be private' do
