5b4491de2813c6e2f7cdf09107ea3bd74752aab2
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-06 02:12:23 +0900

    typedef spwf_func

--- ext/ruphy_gsl/ruphy_gsl.h
+++ ext/ruphy_gsl/ruphy_gsl.h
@@ -7,4 +7,6 @@ VALUE rb_mGSL;
 void init_SPWF(void);
 void init_SPOP(void);
 
+typedef gsl_complex(*spwf_func)(double r, double theta, double phy, void *params);
+
 VALUE ruphy_gsl2rb_complex(const gsl_complex* orig);
--- ext/ruphy_gsl/spwf.c
+++ ext/ruphy_gsl/spwf.c
@@ -14,8 +14,19 @@ static VALUE rb_cSPWF;
 
 static VALUE rb_cHydrogenic;
 
+typedef struct _hwf_params {
+	unsigned int n;
+	unsigned int l;
+	int          m;
+	unsigned int Z;
+} hwf_params;
+
 static gsl_complex hydrogenic_wave_function(double r, double theta, double phy,
-		unsigned int n, unsigned int l, int m, unsigned int Z) {
+		hwf_params *params) {
+	unsigned int n = params->n;
+	unsigned int l = params->l;
+	int          m = params->m;
+	unsigned int Z = params->Z;
 	return gsl_complex_mul_real( gsl_complex_exp (gsl_complex_rect(0, m * phy) ),
 			sqrt( gsl_pow_3( 2*Z/n / gsl_sf_fact(n+l) ) * gsl_sf_fact(n-l-1) / 2 / n ) *
 			exp(-r*Z) * gsl_sf_pow_int(r, l) * gsl_sf_laguerre_n (n+1, 2l+1, 2*r*Z) *
@@ -23,9 +34,12 @@ static gsl_complex hydrogenic_wave_function(double r, double theta, double phy,
 }
 
 static VALUE return_value(VALUE self, VALUE r, VALUE theta, VALUE phy) {
-	gsl_complex ret = hydrogenic_wave_function(rb_num2dbl(r), rb_num2dbl(theta), rb_num2dbl(phy),
-			NUM2INT(rb_iv_get(self, "@n")), NUM2INT(rb_iv_get(self, "@l")), NUM2INT(rb_iv_get(self, "@m")),
-			NUM2INT(rb_iv_get(self, "@Z")));
+	hwf_params params;
+	params.n = NUM2INT(rb_iv_get(self, "@n"));
+	params.l = NUM2INT(rb_iv_get(self, "@l"));
+	params.m = NUM2INT(rb_iv_get(self, "@m"));
+	params.Z = NUM2INT(rb_iv_get(self, "@Z"));
+	gsl_complex ret = hydrogenic_wave_function(rb_num2dbl(r), rb_num2dbl(theta), rb_num2dbl(phy),&params);
 	return rb_funcall(rb_mRuPHY, rb_intern("complex"), 2, rb_float_new(GSL_REAL(ret)), rb_float_new(GSL_IMAG(ret)));
 }
 
