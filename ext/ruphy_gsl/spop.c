b9d3800206be85a89effdbe3301fb64f2d605b2d
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-06 06:32:24 +0900

    dummy functions for derivation

--- ext/ruphy_gsl/spop.c
+++ ext/ruphy_gsl/spop.c
@@ -4,6 +4,36 @@ static VALUE rb_cSPOP;
 static VALUE rb_cHamiltonian;
 static VALUE rb_cHydrogenic;
 
+typedef struct {
+	spwf_func func;
+	double x1,x2;
+	void *params;
+} dummy_params;
+
+double r_deriv_dummy_real(double r, dummy_params *params) {
+	return GSL_REAL(params->func(r, params->x1, params->x2, params->params));
+}
+
+double r_deriv_dummy_imag(double r, dummy_params *params) {
+	return GSL_IMAG(params->func(r, params->x1, params->x2, params->params));
+}
+
+double theta_deriv_dummy_real(double theta, dummy_params *params) {
+	return GSL_REAL(params->func(params->x2, theta, params->x1, params->params));
+}
+
+double theta_deriv_dummy_imag(double theta, dummy_params *params) {
+	return GSL_IMAG(params->func(params->x2, theta, params->x1, params->params));
+}
+
+double phy_deriv_dummy_real(double phy, dummy_params *params) {
+	return GSL_REAL(params->func(params->x1, params->x2, phy, params->params));
+}
+
+double phy_deriv_dummy_imag(double phy, dummy_params *params) {
+	return GSL_IMAG(params->func(params->x1, params->x2, phy, params->params));
+}
+
 void init_SPOP(void) {
 	rb_cSPOP        = rb_define_class_under(rb_mGSL, "SPOP", rb_cObject);
 	rb_cHamiltonian = rb_define_class_under(rb_cSPOP, "Hamiltonian", rb_cSPOP);
