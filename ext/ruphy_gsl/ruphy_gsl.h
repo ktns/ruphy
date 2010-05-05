b64cd4bca865dbadbcf38ebe6153c51fe607f953
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-06 03:30:52 +0900

    typedef spop_func

--- ext/ruphy_gsl/ruphy_gsl.h
+++ ext/ruphy_gsl/ruphy_gsl.h
@@ -8,5 +8,6 @@ void init_SPWF(void);
 void init_SPOP(void);
 
 typedef gsl_complex(*spwf_func)(double r, double theta, double phy, void *params);
+typedef gsl_complex(*spop_func)(double r, double theta, double phy, void *op_params, spwf_func spwf, void *wf_params);
 
 VALUE ruphy_gsl2rb_complex(const gsl_complex* orig);
