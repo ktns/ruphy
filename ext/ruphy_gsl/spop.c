20c191e8319e86b96fcbce1ac042b0f2974e5c90
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-09 04:27:13 +0900

    defined C function `hamiltonian'

--- ext/ruphy_gsl/spop.c
+++ ext/ruphy_gsl/spop.c
@@ -1,10 +1,31 @@
 #include "ruphy_gsl.h"
-
 #include "spop.h"
+
+#include <gsl/gsl_complex_math.h>
 
 static VALUE rb_mSPOP;
 static VALUE rb_mHamiltonian;
 static VALUE rb_cHydrogenic;
+
+typedef struct {
+	spop_func  potential_func;
+	void      *potential_params;
+}potential_params;
+
+static gsl_complex hamiltonian(double r, double theta, double phy,
+		void *op_params, spwf_func spwf, void *wf_params)
+{
+	potential_params *p_params = op_params;
+
+	gsl_complex ret_laplacian =
+		spop_laplacian(r, theta, phy, NULL, spwf, wf_params);
+
+	gsl_complex ret_potential = p_params->potential_func(
+			r, theta,phy, p_params->potential_params, spwf, wf_params);
+
+	return gsl_complex_add(
+			gsl_complex_div_real(ret_laplacian, 2), ret_potential);
+}
 
 void init_SPOP(void) {
 	rb_mSPOP        = rb_define_module_under(rb_mGSL, "SPOP");
