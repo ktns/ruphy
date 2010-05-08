91b4af09f1e428ea8e6ec749d514dcfcc9e7fe46
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-09 00:30:35 +0900

    defined C function `spop_laplacian'

--- ext/ruphy_gsl/spop.h
+++ ext/ruphy_gsl/spop.h
@@ -7,4 +7,5 @@ gsl_complex spop_r_deriv    ( double r, double theta, double phy, void *op_param
 gsl_complex spop_theta_deriv( double r, double theta, double phy, void *op_params, spwf_func spwf, void *wf_params );
 gsl_complex spop_phy_deriv  ( double r, double theta, double phy, void *op_params, spwf_func spwf, void *wf_params );
 
+gsl_complex spop_laplacian  ( double r, double theta, double phy, void *op_params, spwf_func spwf, void *wf_params );
 #endif //_SPOP_H_
