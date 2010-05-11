66938f18aac398f3c66a5ebfadc6ad62b7f21782
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-11 10:39:41 +0900

    fixed bugs in hydrogenic wave function

--- ext/ruphy_gsl/spwf.c
+++ ext/ruphy_gsl/spwf.c
@@ -31,9 +31,10 @@ static gsl_complex hydrogenic_wave_function(double r, double theta, double phy,
 	int          m = hwf_params->m;
 	unsigned int Z = hwf_params->Z;
 	return gsl_complex_mul_real( gsl_complex_exp (gsl_complex_rect(0, m * phy) ),
-			sqrt( gsl_pow_3( 2*Z/n / gsl_sf_fact(n+l) ) * gsl_sf_fact(n-l-1) / 2 / n ) *
-			exp(-r*Z) * gsl_sf_pow_int(r, l) * gsl_sf_laguerre_n (n+1, 2l+1, 2*r*Z) *
-			( m>0 ? 2*GSL_IS_EVEN(m)-1 : 1 ) * gsl_sf_legendre_sphPlm(l, abs(m), cos(theta)));
+			sqrt( gsl_pow_3( 2*Z/n / gsl_sf_fact(n+l) ) * (l + 1.0/2) * gsl_sf_fact(n-l-1)
+				* gsl_sf_fact(l-abs(m)) / 2 / n / 2 / M_PI / gsl_sf_fact(l+abs(m)) ) *
+			exp(-r*Z/n) * gsl_sf_pow_int(r, l) * gsl_sf_laguerre_n (n+l, 2l+1, 2*r*Z/n) *
+			( m>0 && GSL_IS_ODD(m) ? -1 : 1 ) * gsl_sf_legendre_sphPlm(l, abs(m), cos(theta)));
 }
 
 static VALUE get_func_hydrogenic(VALUE self)
