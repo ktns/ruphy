baee1f64228ad3afd74675d159be314634741635
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-11 18:32:50 +0900

    defined get_func_param
    extract func and params from instance of SPWF

--- ext/ruphy_gsl/spwf.c
+++ ext/ruphy_gsl/spwf.c
@@ -3,11 +3,16 @@
 
 VALUE rb_mSPWF;
 
+static void get_func_param(VALUE spwf, spwf_func **func, void** params)
+{
+	Data_Get_Struct(rb_funcall(spwf, rb_intern("get_func"), 0), spwf_func, *func);
+	Data_Get_Struct(rb_iv_get(spwf, "params"), void, *params);
+}
+
 static VALUE return_value(VALUE self, VALUE r, VALUE theta, VALUE phy) {
 	void *params;
 	spwf_func *func;
-	Data_Get_Struct(rb_iv_get(self, "params"), void, params);
-	Data_Get_Struct(rb_funcall(self, rb_intern("get_func"), 0), spwf_func, func);
+	get_func_param(self, &func, &params);
 	gsl_complex ret = (*func)(rb_num2dbl(r), rb_num2dbl(theta), rb_num2dbl(phy),params);
 	return rb_funcall(rb_mRuPHY, rb_intern("complex"), 2, rb_float_new(GSL_REAL(ret)), rb_float_new(GSL_IMAG(ret)));
 }
