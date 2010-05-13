19486d8ef71e56cb54c8d047acb06dbfd1a5f039
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-13 13:35:39 +0900

    use gsl_sf_hydrogenicR for spwf_hydrogenic

--- ext/ruphy_gsl/spwf_hydrogenic.c
+++ ext/ruphy_gsl/spwf_hydrogenic.c
@@ -5,11 +5,8 @@
 
 #include <gsl/gsl_complex_math.h>
 #include <gsl/gsl_math.h>
-#include <gsl/gsl_pow_int.h>
-#include <gsl/gsl_sf_gamma.h>
-#include <gsl/gsl_sf_laguerre.h>
+#include <gsl/gsl_sf_coulomb.h>
 #include <gsl/gsl_sf_legendre.h>
-#include <gsl/gsl_sf_pow_int.h>
 
 static VALUE rb_cHydrogenic;
 static VALUE rb_cHydrogenic_Params;
@@ -30,9 +27,8 @@ static gsl_complex hydrogenic_wave_function(double r, double theta, double phy,
 	int          m = hwf_params->m;
 	unsigned int Z = hwf_params->Z;
 	return gsl_complex_mul_real( gsl_complex_exp (gsl_complex_rect(0, m * phy) ),
-			sqrt( gsl_pow_3( 2*Z/n / gsl_sf_fact(n+l) ) * gsl_sf_fact(n-l-1) / 2 / n ) *
-			exp(-r*Z/n) * gsl_sf_pow_int(r, l) * gsl_sf_laguerre_n (n+l, 2*l+1, 2*r*Z/n) *
-			( m>0 && GSL_IS_ODD(m) ? -1 : 1 ) * gsl_sf_legendre_sphPlm(l, abs(m), cos(theta)));
+			gsl_sf_hydrogenicR(n, l, Z, r) * ( m>0 && GSL_IS_ODD(m) ? -1 : 1 )
+			* gsl_sf_legendre_sphPlm(l, abs(m), cos(theta)));
 }
 
 static VALUE get_func_hydrogenic(VALUE self)
