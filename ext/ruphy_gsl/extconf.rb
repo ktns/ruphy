e7a8b04bb15e0c9bfa677691b0fc5c0f0fe02910
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-20 12:19:28 +0900

    use split and join to change CFLAGS in extconf.rb

--- ext/ruphy_gsl/extconf.rb
+++ ext/ruphy_gsl/extconf.rb
@@ -2,9 +2,15 @@ require 'mkmf'
 
 dir_config("ruphy_gsl")
 
+$CFLAGS = $CFLAGS.split(/\s+/)
+
 if enable_config('deriv-test')
-	$CFLAGS += '-DRUPHY_DERIV_TEST '
+	$CFLAGS << '-DRUPHY_DERIV_TEST'
 end
+
+$CFLAGS << '-Wall'
+
+$CFLAGS = $CFLAGS.join(' ')
 
 def gsl_check
 	find_library('gsl', 'gsl_sf_legendre_Plm') or
@@ -15,7 +21,5 @@ end
 unless gsl_check
 	raise 'Extention build dependency is not met! Please install libgsl!'
 end
-
-$CFLAGS += '-Wall'
 
 create_makefile("ruphy_gsl")
