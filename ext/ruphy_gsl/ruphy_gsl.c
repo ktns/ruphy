f6b604c96d86569ec052b843b7e43f047ea3758c
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-21 08:21:44 +0900

    defined module GSLError::Errno

--- ext/ruphy_gsl/ruphy_gsl.c
+++ ext/ruphy_gsl/ruphy_gsl.c
@@ -8,6 +8,8 @@ VALUE rb_mRuPHY;
 VALUE rb_mGSL;
 VALUE rb_cGSLError;
 
+static VALUE rb_mGSLERRNO;
+
 static void gsl_errror_handler(const char *reason, const char *file, int line, int gsl_errno)
 {
 	VALUE exc_arg[4] = { rb_str_new2(reason), rb_str_new2(file), rb_int_new(line), rb_int_new(gsl_errno)};
@@ -35,6 +37,43 @@ void Init_ruphy_gsl()
 	rb_mRuPHY    = rb_define_module("RuPHY");
 	rb_mGSL      = rb_define_module_under(rb_mRuPHY, "GSL");
 	rb_cGSLError = rb_define_class_under(rb_mGSL, "GSLError", rb_eException);
+	rb_mGSLERRNO = rb_define_module_under(rb_cGSLError, "Errno");
+
+	rb_define_const(rb_mGSLERRNO, "GSL_SUCCESS" , rb_int_new(GSL_SUCCESS));
+	rb_define_const(rb_mGSLERRNO, "GSL_FAILURE" , rb_int_new(GSL_FAILURE));
+	rb_define_const(rb_mGSLERRNO, "GSL_CONTINUE", rb_int_new(GSL_CONTINUE));
+	rb_define_const(rb_mGSLERRNO, "GSL_EDOM"    , rb_int_new(GSL_EDOM));
+	rb_define_const(rb_mGSLERRNO, "GSL_ERANGE"  , rb_int_new(GSL_ERANGE));
+	rb_define_const(rb_mGSLERRNO, "GSL_EFAULT"  , rb_int_new(GSL_EFAULT));
+	rb_define_const(rb_mGSLERRNO, "GSL_EINVAL"  , rb_int_new(GSL_EINVAL));
+	rb_define_const(rb_mGSLERRNO, "GSL_EFAILED" , rb_int_new(GSL_EFAILED));
+	rb_define_const(rb_mGSLERRNO, "GSL_EFACTOR" , rb_int_new(GSL_EFACTOR));
+	rb_define_const(rb_mGSLERRNO, "GSL_ESANITY" , rb_int_new(GSL_ESANITY));
+	rb_define_const(rb_mGSLERRNO, "GSL_ENOMEM"  , rb_int_new(GSL_ENOMEM));
+	rb_define_const(rb_mGSLERRNO, "GSL_EBADFUNC", rb_int_new(GSL_EBADFUNC));
+	rb_define_const(rb_mGSLERRNO, "GSL_ERUNAWAY", rb_int_new(GSL_ERUNAWAY));
+	rb_define_const(rb_mGSLERRNO, "GSL_EMAXITER", rb_int_new(GSL_EMAXITER));
+	rb_define_const(rb_mGSLERRNO, "GSL_EZERODIV", rb_int_new(GSL_EZERODIV));
+	rb_define_const(rb_mGSLERRNO, "GSL_EBADTOL" , rb_int_new(GSL_EBADTOL));
+	rb_define_const(rb_mGSLERRNO, "GSL_ETOL"    , rb_int_new(GSL_ETOL));
+	rb_define_const(rb_mGSLERRNO, "GSL_EUNDRFLW", rb_int_new(GSL_EUNDRFLW));
+	rb_define_const(rb_mGSLERRNO, "GSL_EOVRFLW" , rb_int_new(GSL_EOVRFLW));
+	rb_define_const(rb_mGSLERRNO, "GSL_ELOSS"   , rb_int_new(GSL_ELOSS));
+	rb_define_const(rb_mGSLERRNO, "GSL_EROUND"  , rb_int_new(GSL_EROUND));
+	rb_define_const(rb_mGSLERRNO, "GSL_EBADLEN" , rb_int_new(GSL_EBADLEN));
+	rb_define_const(rb_mGSLERRNO, "GSL_ENOTSQR" , rb_int_new(GSL_ENOTSQR));
+	rb_define_const(rb_mGSLERRNO, "GSL_ESING"   , rb_int_new(GSL_ESING));
+	rb_define_const(rb_mGSLERRNO, "GSL_EDIVERGE", rb_int_new(GSL_EDIVERGE));
+	rb_define_const(rb_mGSLERRNO, "GSL_EUNSUP"  , rb_int_new(GSL_EUNSUP));
+	rb_define_const(rb_mGSLERRNO, "GSL_EUNIMPL" , rb_int_new(GSL_EUNIMPL));
+	rb_define_const(rb_mGSLERRNO, "GSL_ECACHE"  , rb_int_new(GSL_ECACHE));
+	rb_define_const(rb_mGSLERRNO, "GSL_ETABLE"  , rb_int_new(GSL_ETABLE));
+	rb_define_const(rb_mGSLERRNO, "GSL_ENOPROG" , rb_int_new(GSL_ENOPROG));
+	rb_define_const(rb_mGSLERRNO, "GSL_ENOPROGJ", rb_int_new(GSL_ENOPROGJ));
+	rb_define_const(rb_mGSLERRNO, "GSL_ETOLF"   , rb_int_new(GSL_ETOLF));
+	rb_define_const(rb_mGSLERRNO, "GSL_ETOLX"   , rb_int_new(GSL_ETOLX));
+	rb_define_const(rb_mGSLERRNO, "GSL_ETOLG"   , rb_int_new(GSL_ETOLG));
+	rb_define_const(rb_mGSLERRNO, "GSL_EOF"     , rb_int_new(GSL_EOF));
 
 #ifdef RUPHY_GSL_ERROR_TEST
 	rb_define_singleton_method(rb_cGSLError, "gsl_error_test", gsl_error_test, 0);
