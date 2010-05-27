4947abe8fdf9d727f00d2f9978f99842d64c4565
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-28 07:21:50 +0900

    spec: SPWF::Multiplied identical pair

---
+++ spec/spwf_operated_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+module RuPHY::GSL
+	module SPWF
+		class Multiplied
+			describe Multiplied do
+				describe 'identical pair' do
+					before do
+						@spwfs = [[
+							Hydrogenic.new(1,0,0,1) * -1,
+							- Hydrogenic.new(1,0,0,1)
+						]]
+					end
+
+					it 'should have same hash' do
+						@spwfs.each do |spwf1,spwf2|
+							spwf1.hash.should == spwf2.hash
+						end
+					end
+
+					it 'should eql' do
+						@spwfs.each do |spwf1,spwf2|
+							spwf1.should eql spwf2
+						end
+					end
+
+					it 'should ==' do
+						@spwfs.each do |spwf1,spwf2|
+							spwf1.should == spwf2
+						end
+					end
+				end
+			end
+		end
+	end
+end
