56fcedd71694ca1426641746217c3176a235800c
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-27 15:14:28 +0900

    defined SPOP#*

--- lib/ruphy/gsl/spop.rb
+++ lib/ruphy/gsl/spop.rb
@@ -1,6 +1,14 @@
 module RuPHY::GSL
 	# Represents an operator which acts on single particle wave functions.
 	module SPOP
+		def * other
+			case other
+			when SPWF
+				return SPWF::Operated.new self, other
+			else
+				raise NotImplementedError, "#{self.class} * #{other.class} is not implemented!"
+			end
+		end
 	end
 end
 require 'ruphy/gsl/spop/hamiltonian'
--- spec/spop_spec.rb
+++ spec/spop_spec.rb
@@ -2,6 +2,19 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 module RuPHY::GSL
 	module SPOP
+		describe SPOP do
+			describe '#*'do
+				it 'should invoke SPWF::Operated' do
+					@spop = mock(:spop)
+					@spop.extend SPOP
+					@spwf = mock(:spwf)
+					@spwf.extend SPWF
+					SPWF::Operated.should_receive(:new).with(@spop, @spwf)
+					@spop * @spwf
+				end
+			end
+		end
+
 		module Hamiltonian
 			describe Hydrogenic do
 				it 'should respond_to :new' do
