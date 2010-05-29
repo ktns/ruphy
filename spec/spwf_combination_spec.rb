531b65bb7d4dfa2289b51cb4418e9abc03d10380
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-30 04:20:20 +0900

    spec: SPWF::Combination#eval
    Yet implemented

---
+++ spec/spwf_combination_spec.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+module RuPHY::GSL
+	module SPWF
+		class Combination
+			describe Combination do
+				describe '#eval' do
+					it 'should return summation of operands'
+				end
+			end
+		end
+	end
+end
