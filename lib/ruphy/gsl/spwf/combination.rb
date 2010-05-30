341637ed5a92fb46939c432366f79a1fe50b5f65
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-30 22:37:40 +0900

    argument validation at SPWF::Combination#initialize

--- lib/ruphy/gsl/spwf/combination.rb
+++ lib/ruphy/gsl/spwf/combination.rb
@@ -5,6 +5,12 @@ module RuPHY
 				include SPWF
 
 				def initialize *args
+					[nil,false].each do |n|
+						raise ArgumentError, "#{SPWF} expected, but#{n}!" if args.include?(n)
+					end
+					err = args.find{|arg|not arg.kind_of?(SPWF)}
+					raise TypeError, "#{SPWF} expected, but #{err.class}!" if err
+
 					setup_params *args
 				end
 			end
