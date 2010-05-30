b500985f893842499390596a49d5f08895237d94
Katsuhiko Nishimra <kat841@hotmail.com>
2010-05-30 19:24:21 +0900

    spec: SPWF::Combination#eval

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,9 @@ rescue LoadError
 	require 'spec'
 end
 
+def random_coordinate
+	return (1-a=rand())/a, rand() * Math::PI/2, rand() * Math::PI
+end
+
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'ruphy'
--- spec/spwf_combination_spec.rb
+++ spec/spwf_combination_spec.rb
@@ -5,7 +5,18 @@ module RuPHY::GSL
 		class Combination
 			describe Combination do
 				describe '#eval' do
-					it 'should return summation of operands'
+					before do
+						@spwf1, @spwf2 = Hydrogenic.new(2,1,1,1),Hydrogenic.new(2,1,-1,1)
+						@spwf = @spwf1 + @spwf2
+					end
+
+					it 'should return summation of operands' do
+						10.times do
+							r, theta, phy = random_coordinate()
+							@spwf.eval(r,theta,phy).should ==
+								@spwf1.eval(r,theta,phy) + @spwf2.eval(r,theta,phy)
+						end
+					end
 				end
 			end
 		end
