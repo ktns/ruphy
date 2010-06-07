f1017861961b12e9528c7a6bf99481aa0a9d806e
Katsuhiko Nishimra <kat841@hotmail.com>
2010-06-07 14:37:17 +0900

    spec: SPOP::Combination * SPWF

--- spec/spop_combination_spec.rb
+++ spec/spop_combination_spec.rb
@@ -3,6 +3,24 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 module RuPHY::GSL
 	module SPOP
 		describe Combination do
+			before do
+				@spops = Array.new(4){Multiplier.new(rand())}
+				@spop_combination = Combination.new(*@spops)
+			end
+
+			describe 'operated on a function' do
+				before do
+					@spwf = random_spwf_hydrogenic
+				end
+
+				it 'should return summation of original operators operated on the function' do
+					10.times do
+						coord = random_coordinate
+						(@spop_combination * @spwf).eval(*coord).should ==
+							@spops.inject(0){|s,o| s + (o * @spwf).eval(*coord)}
+					end
+				end
+			end
 		end
 	end
 end
