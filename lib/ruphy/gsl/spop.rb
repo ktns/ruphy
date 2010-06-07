e8ef0f6551527b90d8eb20f29d51fcff9675dcf5
Katsuhiko Nishimra <kat841@hotmail.com>
2010-06-07 14:56:29 +0900

    defined SPOP + SPOP

--- lib/ruphy/gsl/spop.rb
+++ lib/ruphy/gsl/spop.rb
@@ -1,6 +1,15 @@
 module RuPHY::GSL
 	# Represents an operator which acts on single particle wave functions.
 	module SPOP
+		def + other
+			case other
+			when SPOP
+				Combination.new(self, other)
+			else
+				raise NotImplementedError, "#{self.class} + #{other.class} is not implemented!"
+			end
+		end
+
 		def * other
 			case other
 			when SPWF
