e2b0cc85b9f8485bfcf1a07678af479e75c7c3ab
Katsuhiko Nishimra <kat841@hotmail.com>
2010-06-08 01:38:35 +0900

    define SPOP::Combination identity methods

--- lib/ruphy/gsl/spop/combination.rb
+++ lib/ruphy/gsl/spop/combination.rb
@@ -4,7 +4,23 @@ module RuPHY::GSL
 			include SPOP
 
 			def initialize *spops
-				setup_params *spops
+				@spops = spops.sort_by(&:hash).freeze
+				setup_params *@spops
+			end
+			attr_reader :spops
+
+			def hash
+				@spops.hash
+			end
+
+			def eql? other
+				self.class.eql? other.class and
+				@spops.eql? other.spops
+			end
+
+			def == other
+				self.class == other.class and
+				@spops == other.spops
 			end
 		end
 	end
