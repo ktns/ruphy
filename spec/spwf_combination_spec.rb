5463838b29b6567622e519f673105c39196f637a
Katsuhiko Nishimra <kat841@hotmail.com>
2010-06-08 12:17:07 +0900

    spec: SPWF::Combination with permutated functions

--- spec/spwf_combination_spec.rb
+++ spec/spwf_combination_spec.rb
@@ -4,6 +4,11 @@ module RuPHY::GSL
 	module SPWF
 		class Combination
 			describe Combination do
+				before do
+					@spwfs = Array.new(4){random_spwf_hydrogenic()}.sort_by(&:hash)
+					@spwf_combination = Combination.new(*@spwfs)
+				end
+
 				describe '.new' do
 					it 'should raise error if invalid argument specified' do
 						[nil, false].each do |n|
@@ -18,16 +23,48 @@ module RuPHY::GSL
 				end
 
 				describe '#eval' do
-					before do
-						@spwf1, @spwf2 = Hydrogenic.new(2,1,1,1),Hydrogenic.new(2,1,-1,1)
-						@spwf = @spwf1 + @spwf2
-					end
-
 					it 'should return summation of operands' do
 						10.times do
-							r, theta, phy = random_coordinate()
-							@spwf.eval(r,theta,phy).should ==
-								@spwf1.eval(r,theta,phy) + @spwf2.eval(r,theta,phy)
+							begin
+								r, theta, phy = random_coordinate()
+								@spwf_combination.eval(r,theta,phy).should ==
+									@spwfs.inject(0){|s, f| s + f.eval(r,theta,phy)}
+							rescue GSLError
+								retry if $!.errno == GSLError::Errno::GSL_EUNDRFLW
+								raise $!
+							end
+						end
+					end
+				end
+
+				describe 'with permutated functions' do
+					before do
+						@spwf_combinations = @spwfs.permutation.collect do |spwfs|
+							Combination.new(*spwfs)
+						end
+					end
+
+					it 'should not be equal each other' do
+						@spwf_combinations.combination(2) do |spwf1, spwf2|
+							spwf1.should_not equal spwf2
+						end
+					end
+
+					it 'should be eql each other' do
+						@spwf_combinations.combination(2) do |spwf1, spwf2|
+							spwf1.should eql spwf2
+						end
+					end
+
+					it 'should be == each other' do
+						@spwf_combinations.combination(2) do |spwf1, spwf2|
+							spwf1.should == spwf2
+						end
+					end
+
+					it 'should have same hash as aother' do
+						@spwf_combinations.combination(2) do |spwf1, spwf2|
+							spwf1.hash.should == spwf2.hash
 						end
 					end
 				end
