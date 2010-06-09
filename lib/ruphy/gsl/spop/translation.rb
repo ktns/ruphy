20b0a5bed1d22d98226c51ba335b10588a482477
Katsuhiko Nishimra <kat841@hotmail.com>
2010-06-09 18:25:49 +0900

    defined SPOP::Translation#inverse

--- lib/ruphy/gsl/spop/translation.rb
+++ lib/ruphy/gsl/spop/translation.rb
@@ -4,7 +4,13 @@ module RuPHY::GSL
 			include SPOP
 
 			def initialize dx, dy, dz
+				@dx, @dy, @dz = dx, dy, dz
 				setup_params dx, dy, dz
+			end
+			attr_reader :dx, :dy, :dz
+
+			def inverse
+				self.class.new -@dx, -@dy, -@dz 
 			end
 		end
 	end
