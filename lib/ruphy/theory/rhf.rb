41f20dc9f5d349a060be928db449768cbee0b6df
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-03-15 07:17:12 +0900

    RHF class skeletons

--- lib/ruphy/theory/rhf.rb
+++ lib/ruphy/theory/rhf.rb
@@ -1,7 +1,31 @@
+require 'matrix'
+
 module RuPHY
 	module Theory
 		module RHF
+			class CoefficientMatrix < Matrix
 
+			end
+
+			class FockianMatrix < Matrix
+
+			end
+
+			class OverlapMatrix < Matrix
+
+			end
+
+			class OneElectronMatrix < Matrix
+
+			end
+
+			class TwoElectronMatrix < Matrix
+
+			end
+
+			class Solver
+
+			end
 		end
 	end
 end
