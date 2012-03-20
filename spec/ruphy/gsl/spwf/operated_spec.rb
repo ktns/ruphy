c7f5f5f18d76ffd05e5a4e8168c828b685742a71
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-03-21 05:06:45 +0900

    New spec for SPWF::Operated

--- spec/ruphy/gsl/spwf/operated_spec.rb
+++ spec/ruphy/gsl/spwf/operated_spec.rb
@@ -1,6 +1,17 @@
 require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 
-module RuPHY::GSL
-	module SPWF
+describe RuPHY::GSL::SPWF::Operated do
+	before do
+		@op = RuPHY::GSL::SPOP::Multiplier.new(1)
+		@wf = RuPHY::GSL::SPWF::Hydrogenic.new(1,0,0,1);
+	end
+
+	subject {described_class.new(@op, @wf)}
+
+	it 'should have proper instance variables' do
+		class << subject
+			@spwf.should == @wf
+			@spop.should == @op
+		end
 	end
 end
