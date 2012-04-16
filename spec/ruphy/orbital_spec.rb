30788281ff0ffde57918fadb6980058084a1e05e
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-04-16 09:41:03 +0900

    New spec: Orbital.new should raise error

---
+++ spec/ruphy/orbital_spec.rb
@@ -0,0 +1,9 @@
+require File.join(File.dirname(__FILE__), %w|.. spec_helper.rb|)
+
+describe RuPHY::Orbital do
+	describe '.new' do
+		subject{lambda{described_class.new}}
+
+		it{should raise_error NotImplementedError}
+	end
+end
