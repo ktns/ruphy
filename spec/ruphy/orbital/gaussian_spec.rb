7a4f2ed8eec262a783c3787cf39437cdec7a7477
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-04-17 15:49:08 +0900

    New spec for Gaussian::Primitive.new

---
+++ spec/ruphy/orbital/gaussian_spec.rb
@@ -0,0 +1,15 @@
+require File.join(File.dirname(__FILE__), %w'.. .. spec_helper.rb')
+
+describe RuPHY::Orbital::Gaussian do
+
+end
+
+describe RuPHY::Orbital::Gaussian::Primitive do
+	describe '.new' do
+		it 'should accept zeta, orders, and center' do
+			lambda do
+				described_class.new 1.0, [0,0,0], Vector[0,0,0]
+			end.should_not raise_error
+		end
+	end
+end
