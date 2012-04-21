36441fb245f95ef9564c8ae880a87547dcfb300b
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-04-21 16:52:47 +0900

    New spec for Gaussian::Contracted.new

--- spec/ruphy/orbital/gaussian_spec.rb
+++ spec/ruphy/orbital/gaussian_spec.rb
@@ -25,3 +25,13 @@ describe RuPHY::Orbital::Gaussian::Primitive do
 		end
 	end
 end
+
+describe RuPHY::Orbital::Gaussian::Contracted do
+	describe '.new' do
+		it 'should accept coeffs, zetas, orders, and center' do
+			lambda do
+				described_class.new [1.0,0.0], [1.0,2.0], [0,0,0], Vector[0,0,0]
+			end.should_not raise_error
+		end
+	end
+end
