3a72308f0d08959b40d526ed55747a5cdc91717b
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-05-11 20:45:01 +0900

    Freeze attributes of Gaussian::Primitive

--- lib/ruphy/orbital/gaussian.rb
+++ lib/ruphy/orbital/gaussian.rb
@@ -7,9 +7,9 @@ module RuPHY
 
 			class Primitive < Orbital
 				def initialize zeta, momenta, center
-					@zeta, @momenta, @center = zeta.to_f,
-						(momenta.to_ary rescue momenta.to_a), \
-						Vector[*center]
+					@zeta, @momenta, @center = [zeta.to_f,
+						(momenta.to_ary rescue momenta.to_a),
+						Vector[*center]].map(&:freeze)
 					raise ArgumentError, 'Invalid value for a zeta(%p)!' % zeta unless @zeta > 0
 					raise ArgumentError, 'Invalid size of momenta(%d)!' % @momenta.size unless @momenta.size == 3
 					@momenta.each do |m|
