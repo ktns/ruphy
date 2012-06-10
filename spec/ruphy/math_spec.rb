86b47aa25ae9cd46d8062b40b1c4b7dc12059d9b
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-06-10 09:26:24 +0900

    refactor spec for Boys function

--- spec/ruphy/math_spec.rb
+++ spec/ruphy/math_spec.rb
@@ -5,9 +5,9 @@ describe RuPHY::Math do
 		subject{described_class.boys(r,m)}
 
 		context 'r=0,m=0' do
-			let(:r){0};let(:m){0}
+			let(:r){Float::EPSILON};let(:m){0}
 
-			it {should == 1}
+			it {should be_within(1e-5).of(1)}
 		end
 	end
 end
