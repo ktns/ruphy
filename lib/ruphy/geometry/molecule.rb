886e0367b723b9565d01f54d1df9169219478dfa
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-09-28 16:14:28 +0900

    Call Reader::XYZ#read in Molecule#initialize

--- lib/ruphy/geometry/molecule.rb
+++ lib/ruphy/geometry/molecule.rb
@@ -1,9 +1,23 @@
 require 'ruphy'
+require 'ruphy/geometry/reader/xyz'
 
 module RuPHY
 	module Geometry
 		class Molecule
+			def initialize *args
+				if File.exist? fname=args.first
+					case File.extname(fname)
+					when /xyz/i
+						extend Reader::XYZ
+						read fname
+						return
+					else
+						raise ArgumentError
+					end
+				end
 
+				raise ArgumentError
+			end
 		end
 	end
 end
