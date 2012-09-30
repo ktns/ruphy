323d68694b28889f16d0a81d8d5d57a89b1aa4e3
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-10-01 00:42:17 +0900

    Fix rspec message

--- spec/ruphy/elements_spec.rb
+++ spec/ruphy/elements_spec.rb
@@ -1,10 +1,10 @@
 require File.join(File.dirname(__FILE__), %w|.. spec_helper.rb|)
 
-describe RuPHY::Elements do
+describe 'RuPHY::Elements' do
 	subject do
 		RuPHY::Elements
 	end
-	describe '[]' do
+	describe '#[]' do
 		it 'should invoke get_elem_data' do
 			subject.should_receive(:get_elem_data).and_return([0,:hoge,0])
 			RuPHY::Elements[:hoge] 
