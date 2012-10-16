c05305e4da796b76d66937aede64b6b5a69275c1
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-10-17 01:31:18 +0900

    Define constant Angstrom

--- lib/ruphy/constants.rb
+++ lib/ruphy/constants.rb
@@ -2,11 +2,13 @@ module RuPHY
 	module Constants
 		module Predefined
 			module SI
-				Da = 1.66053892173e-27
-				Me = 9.109382616e-31
+				Da   = 1.66053892173e-27
+				Me   = 9.109382616e-31
+				Bohr = 0.5291772109217e-10
 			end
 
 			Da = SI::Da / SI::Me
+			Angstrom = 1e-10 / SI::Bohr
 		end
 
 		def self.const_missing name
