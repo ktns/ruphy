502de57a8099f374c4b49e7809eadae71e6a2ce7
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-10-16 14:09:26 +0900

    Implement Geometry::Molecule#initialize

--- lib/ruphy/geometry.rb
+++ lib/ruphy/geometry.rb
@@ -3,8 +3,15 @@ require 'openbabel'
 module RuPHY
 	module Geometry
 		class Molecule
-			def initialize source
-				raise NotImplementedError
+			def initialize source, format = 'xyz'
+				obconv = OpenBabel::OBConversion.new
+				obconv.set_in_format(format) rescue raise ArgumentError, '%p is not a format registered in OpenBabel!'
+				@obmol = OpenBabel::OBMol.new
+				if File.exist? source
+					obconv.read_file(@obmol, source)
+				else
+					obconv.read_string(@obmol, source)
+				end
 			end
 		end
 	end
