66ec6fc7ac6460c4dbedf23b2a066e020317cf9e
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-10-16 15:23:39 +0900

    Fix bug in an error message

--- lib/ruphy/geometry.rb
+++ lib/ruphy/geometry.rb
@@ -5,7 +5,7 @@ module RuPHY
 		class Molecule
 			def initialize source, format = 'xyz'
 				obconv = OpenBabel::OBConversion.new
-				obconv.set_in_format(format) or raise ArgumentError, '%p is not a format registered in OpenBabel!'
+				obconv.set_in_format(format) or raise ArgumentError, '%p is not a format registered in OpenBabel!' % format
 				@obmol = OpenBabel::OBMol.new
 				if File.exist? source
 					obconv.read_file(@obmol, source)
