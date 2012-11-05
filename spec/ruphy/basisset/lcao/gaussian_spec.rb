055e4ec2f78ccfd9a80656b72381cf33d65eec80
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-11-05 13:22:42 +0900

    Fix bugs in stub creation

--- spec/ruphy/basisset/lcao/gaussian_spec.rb
+++ spec/ruphy/basisset/lcao/gaussian_spec.rb
@@ -5,13 +5,13 @@ describe RuPHY::BasisSet::LCAO::Gaussian do
 	subject{described_class.new arg}
 
 	context 'with :H' do
-		let(:arg){{:H=>stub}}
+		let(:arg){{:H=>stub(:HShells)}}
 
 		creating_it{should_not raise_error}
 	end
 
 	context 'with :hoge' do
-		let(:arg){{:hoge=>stub}}
+		let(:arg){{:hoge=>stub(:HeShells)}}
 
 		creating_it{should raise_error described_class::InvalidElementError}
 	end
