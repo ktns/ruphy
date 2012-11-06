7c197034648638a3204764857f0764ac659c660e
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-11-07 00:13:40 +0900

    Relocate specs for RuPHY::Element

--- spec/ruphy/elements_spec.rb
+++ spec/ruphy/elements_spec.rb
@@ -1,43 +1,5 @@
 require File.join(File.dirname(__FILE__), %w|.. spec_helper.rb|)
 
-describe RuPHY::Element do
-	let(:element){described_class.new(0,:hoge,0)}
-
-	let(:same_element){described_class.new(0,:hoge,0)}
-
-	let(:other_element){described_class.new(0,:piyo,0)}
-
-	subject{element}
-
-	it do
-		should == same_element
-	end
-
-	it do
-		should be_eql same_element
-	end
-
-	it do
-		should_not == other_element
-	end
-
-	it do
-		should_not be_eql other_element
-	end
-
-	describe '#hash' do
-		subject{element.hash}
-
-		it do
-			should == same_element.hash
-		end
-
-		it do
-			should_not == other_element.hash
-		end
-	end
-end
-
 describe 'RuPHY::Elements' do
 	subject do
 		RuPHY::Elements
