4c327656f73d64cbd53035c037eca2f2a7102ee1
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-11-18 23:28:41 +0900

    Fix superclass mismatch

--- lib/ruphy.rb
+++ lib/ruphy.rb
@@ -1,11 +1,11 @@
 $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'complex'
+
 require 'rubygems'
 require 'bundler'
 Bundler.require(:default)
-
-require 'complex'
 
 module RuPHY
 	VERSION = IO.read(File.join(File.dirname(__FILE__), %w<.. VERSION>)).chomp
