15f25d53dda1ecd8b1f1cebf731037cea4e7bed1
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-01-19 19:01:30 +0900

    Move STO3G

--- lib/ruphy/basisset/STO3G.rb
+++ lib/ruphy/basisset/STO3G.rb
@@ -1,23 +1,30 @@
 require 'ruphy/ao/gaussian.rb'
+require 'ruphy/basisset/lcao/gaussian'
 
 module RuPHY
   module BasisSet
-    class STO3G < Base
-      def initialize element, center
-        @bases = []
-        case element
-        when 'H'
-          @bases << AO::Gaussian::Contracted.new(
-            [0.15432897,0.53532814,0.44463454],
-            [3.42525091,0.62391373,0.16885540],
-            [0,0,0],
-            center)
+    module LCAO
+      class Gaussian
+        class STO3G < Base
+          def initialize element, center
+            @bases = []
+            case element
+            when 'H'
+              @bases << AO::Gaussian::Contracted.new(
+                [0.15432897,0.53532814,0.44463454],
+                [3.42525091,0.62391373,0.16885540],
+                [0,0,0],
+                center)
+            end
+          end
+
+          def each &block
+            @bases.each &block
+          end
         end
       end
-
-      def each &block
-        @bases.each &block
-      end
     end
+
+    STO3G = LCAO::Gaussian::STO3G
   end
 end
