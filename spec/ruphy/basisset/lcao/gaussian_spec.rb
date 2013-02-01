35b4a1d2a93c94bb12fc92b1233cb53984202d4f
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-02-01 13:56:11 +0900

    Use shared example group

--- spec/ruphy/basisset/lcao/gaussian_spec.rb
+++ spec/ruphy/basisset/lcao/gaussian_spec.rb
@@ -4,10 +4,14 @@ require 'ruphy/basisset/lcao/gaussian'
 describe RuPHY::BasisSet::LCAO::Gaussian do
   subject{described_class.new arg}
 
+  shared_examples_for :proper_basisset do
+    creating_it{should_not raise_error}
+  end
+
   context 'with :H' do
     let(:arg){{:H=>stub(:HShells)}}
 
-    creating_it{should_not raise_error}
+    it_should_behave_like :proper_basisset
   end
 
   context 'with :hoge' do
