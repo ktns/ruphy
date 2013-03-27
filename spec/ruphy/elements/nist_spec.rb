af6d598173cf0686a98a3aac1d2094054b857876
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-03-27 15:02:39 +0900

    Don't use subject in before clause

--- spec/ruphy/elements/nist_spec.rb
+++ spec/ruphy/elements/nist_spec.rb
@@ -13,20 +13,21 @@ if ENV['TEST_NIST']
       end
 
       context 'with H' do
-        before :all do
-          @z,@sym,@m = subject.send(:get_elem_data,:H)
-        end
+        let(:hdat){subject.send(:get_elem_data,:H)}
+        let(:z){hdat[0]}
+        let(:sym){hdat[1]}
+        let(:m){hdat[2]}
 
         it 'should return proper atomic number' do
-          @z.should == 1
+          z.should == 1
         end
 
         it 'should return proper atomic symbol' do
-          @sym.should == :H
+          sym.should == :H
         end
 
         it 'should return proper atomic weight' do
-          @m.should be_within(0.01).of(1)
+          m.should be_within(0.01).of(1)
         end
       end
     end
