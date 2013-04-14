ef3cabaeb623009db8a5da9abc71369454c3ddf3
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-04-14 16:17:52 +0900

    Define class MO::Basis::LCAO::Shell

--- lib/ruphy/mo/basis/lcao.rb
+++ lib/ruphy/mo/basis/lcao.rb
@@ -5,6 +5,18 @@ module RuPHY
         def initialize geometry, basisset
           raise NotImplementedError
         end
+
+        class Shell < SimpleDelegator
+          def initialize abst_shell, center
+            shell = abst_shell.dup.extend ActualShell
+            shell.center = center
+            __setobj__ shell
+          end
+
+          module ActualShell
+            attr_accessor :center
+          end
+        end
       end
     end
   end
--- spec/ruphy/mo/basis/lcao_spec.rb
+++ spec/ruphy/mo/basis/lcao_spec.rb
@@ -9,3 +9,22 @@ describe RuPHY::MO::Basis::LCAO do
     it{pending{should be_kind_of RuPHY::MO::Basis}}
   end
 end
+
+describe RuPHY::MO::Basis::LCAO::Shell do
+  context 'with mockup center and abstract shell' do
+    let(:center){mock(:center)}
+    let!(:abst_shell){mock(:abst_shell)}
+
+    subject{described_class.new(abst_shell, center)}
+
+    its(:center){should == center}
+
+    describe '#aos' do
+      it "should invoke abst_shell#aos" do
+        abst_shell.stub!(:dup).and_return(abst_shell)
+        abst_shell.should_receive(:aos).and_return(nil)
+        subject.aos
+      end
+    end
+  end
+end
