d52aae22b6d80ea4bd5d0c885cb327857eadf326
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-04-19 18:12:02 +0900

    BasisSet::LCAO::Gaussian#shells accepts String

--- spec/ruphy/basisset/STO3G_spec.rb
+++ spec/ruphy/basisset/STO3G_spec.rb
@@ -25,6 +25,12 @@ describe "RuPHY::BasisSet::STO3G" do
       calling_it{should_not raise_error}
     end
 
+    context "with #{RuPHY::Element}.to_s" do
+      let(:arg){random_element.to_s}
+
+      calling_it{should_not raise_error}
+    end
+
     context "with #{RuPHY::Element}.to_sym" do
       let(:arg){random_element.to_sym}
 
