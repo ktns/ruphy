c3a7651793b5502974b08e55301fe05d985ab96f
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-06-03 20:41:46 +0900

    Wrap ::Matrix with RuPHY::Math::Matrix

--- lib/ruphy/math/matrix.rb
+++ lib/ruphy/math/matrix.rb
@@ -13,8 +13,49 @@ module RuPHY
         each :diagonal, &block
       end
 
+      def inspect
+        __getobj__.inspect.sub('Matrix',self.class.to_s)
+      end
+
+      module MatrixWrapper
+        def method_missing method, *args, &block
+          retval = super
+          loop do
+            case retval
+            when ::Matrix
+              Matrix.class_eval do
+                return new(retval)
+              end
+            when Coercer
+              retval = retval.__getobj__
+            else
+              return retval
+            end
+          end
+        end
+
+        def coerce other
+          other, self_ = super
+          return [Coercer.new(other), self_]
+        end
+
+        def inspect
+          "#<#{self.class}: #{__getobj__.inspect}>"
+        end
+      end
+      include MatrixWrapper
+
+      class Coercer < SimpleDelegator
+        def initialize obj
+          __setobj__ obj
+        end
+
+        include MatrixWrapper
+      end
+
       def coerce other
-        [ other, __getobj__ ]
+        other, self_ = super
+        return [Coercer.new(other), self_]
       end
 
       class << self
