2563d609dea5594bf35fdabfcb17f769df1a75c5
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-06-04 20:34:13 +0900

    Additional spec for RuPHY::Math#boys

--- spec/ruphy/math_spec.rb
+++ spec/ruphy/math_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe RuPHY::Math do
-  describe '.boys' do
-    subject{described_class.boys(r,m)}
+  include described_class
 
-    context 'r=0,m=0' do
-      let(:r){Float::EPSILON};let(:m){0}
+  describe '#boys(r, n)' do
+    subject{boys(r, n)}
+    context 'n=0' do
+      let(:n){0}
 
-      it {should be_within(1e-5).of(1)}
+      context 'r = 0' do
+        let(:r){0}
+        it{should be_within(1e-11).of(1)}
+      end
+
+      context 'r -> +0' do
+        let(:r){Float::EPSILON}
+        it{should be_within(1e-11).of(1)}
+      end
+    end
+
+    context 'with n:Float' do
+      let(:n){1.5}; let(:r){1.0}
+      calling_it{should raise_error ArgumentError}
     end
   end
 end
