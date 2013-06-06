5377ad774b8879f611925363388a6f3dde1e6896
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-06-06 14:02:06 +0900

    Define RHF::MO class

--- lib/ruphy/theory/rhf.rb
+++ lib/ruphy/theory/rhf.rb
@@ -1,8 +1,15 @@
-require 'matrix'
+require 'ruphy/mo/basis'
 
 module RuPHY
   module Theory
     module RHF
+      class MO
+        include RuPHY::MO
+        def initialize geometry, basis, number_of_electrons
+          raise NotImplementedError
+        end
+      end
+
       class Solver
         def initialize geometry, basisset
           raise NotImplementedError
