3ba1a81267ad38570f1e8d139e2349f46a95bf33
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-11-18 02:37:10 +0900

    Use extconf.h

--- ext/ruphy_libint2/extconf.rb
+++ ext/ruphy_libint2/extconf.rb
@@ -4,7 +4,9 @@ dir_config('libint2')
 
 have_library('stdc++')
 
-find_library('int2', 'libint2_init_eri')
-find_header('libint2.h')
+have_library('int2', 'libint2_init_eri')
+have_header('libint2.h')
+have_func('libint2_init_eri')
 
+create_header
 create_makefile('ruphy_libint2')
--- ext/ruphy_libint2/ruphy_libint2.c
+++ ext/ruphy_libint2/ruphy_libint2.c
@@ -1,1 +1,8 @@
+#include "extconf.h"
 #include "ruby.h"
+#ifdef HAVE_LIBINT2_H
+#include "libint2.h"
+#endif
+
+#ifdef HAVE_LIBINT2_INIT_ERI
+#endif
