3a1b7d337a070aad4c777ca5af4bbf9ca85d78d7
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-11-19 15:29:25 +0900

    Define RuPHY::Libint2::compiled?

--- ext/ruphy_libint2/ruphy_libint2.c
+++ ext/ruphy_libint2/ruphy_libint2.c
@@ -4,11 +4,18 @@
 #include "libint2.h"
 #endif
 
+static VALUE return_true(){return Qtrue;};
+static VALUE return_false(){return Qfalse;};
+
 static VALUE RuPHY;
 static VALUE Libint2;
+
 void Init_ruphy_libint2(){
 	RuPHY=rb_define_module("RuPHY");
 	Libint2=rb_define_module_under(RuPHY,"Libint2");
 #ifdef HAVE_LIBINT2_INIT_ERI
+	rb_define_singleton_method(Libint2, "compiled?", return_true,  0);
+#else
+	rb_define_singleton_method(Libint2, "compiled?", return_false, 0);
 #endif
 }
--- spec/ruphy/libint2/libint2_spec.rb
+++ spec/ruphy/libint2/libint2_spec.rb
@@ -1,5 +1,9 @@
 require 'ruphy_libint2'
 
 describe RuPHY::Libint2 do
-
+  if described_class.compiled?
+    #TODO: tests here
+  else
+    $stderr.puts "Libint2 extension is not compiled"
+  end
 end
