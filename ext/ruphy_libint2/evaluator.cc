7e05d5b42ffec6fc49dcf5831f37116a8ea1bbd8
Katsuhiko Nishimra <ktns.87@gmail.com>
2013-11-29 03:06:15 +0900

    Initialize eri evaluator
    [ci skip]

---
+++ ext/ruphy_libint2/evaluator.cc
@@ -0,0 +1,55 @@
+#include <ruby.h>
+#include RUBY_EXTCONF_H
+
+#ifdef HAVE_LIBINT2_H
+#include <libint2.h>
+#include "evaluator.h"
+
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
+static VALUE evaluator_class;
+
+static void free_libint_t(void *p){
+	evaluator_struct *st = (evaluator_struct*)p;
+	if(st->buf != NULL){
+		LIBINT2_PREFIXED_NAME(libint2_cleanup_eri)(&st->erieval);
+	}
+	ruby_xfree(st);
+}
+
+static VALUE new_method(VALUE self, VALUE args){
+	VALUE ret;
+	unsigned int max_am;
+	size_t buf_size;
+	void *heap;
+	evaluator_struct *st = ALLOC(evaluator_struct);
+	st->buf = NULL;
+	ret = Data_Wrap_Struct(self, 0, free_libint_t, st);
+	rb_apply(ret, rb_intern("initialize"), args); // this may not return due to exception
+	max_am = NUM2UINT(rb_ivar_get(self, rb_intern("@max_angular_momentum")));
+	buf_size = LIBINT2_PREFIXED_NAME(libint2_need_memory_eri)(max_am);
+	heap = ruby_xmalloc(sizeof(LIBINT2_REALTYPE[buf_size]));
+	st->buf = new(heap) LIBINT2_REALTYPE[buf_size];
+	LIBINT2_PREFIXED_NAME(libint2_init_eri)(&st->erieval, max_am, st->buf);
+	return ret;
+}
+
+static VALUE initialize(VALUE self, VALUE shell1, VALUE shell2, VALUE shell3, VALUE shell4){
+	unsigned int max_am = 0;
+	rb_ivar_set(self, rb_intern("@shells"), rb_ary_new3(4, shell1, shell2, shell3, shell4));
+	max_am = MAX(max_am, NUM2UINT(rb_apply(shell1, rb_intern("angular_momentum"), Qnil)));
+	max_am = MAX(max_am, NUM2UINT(rb_apply(shell2, rb_intern("angular_momentum"), Qnil)));
+	max_am = MAX(max_am, NUM2UINT(rb_apply(shell3, rb_intern("angular_momentum"), Qnil)));
+	max_am = MAX(max_am, NUM2UINT(rb_apply(shell4, rb_intern("angular_momentum"), Qnil)));
+	rb_ivar_set(self, rb_intern("@max_angular_momentum"), UINT2NUM(max_am));
+	return self;
+}
+
+extern "C"
+void ruphy_libint2_define_evaluator(VALUE libint2_module){
+	evaluator_class = rb_define_class_under(libint2_module, "Evaluator", rb_cObject);
+	rb_define_method(evaluator_class, "initialize", (VALUE(*)(...))initialize, 4);
+	rb_define_singleton_method(evaluator_class, "new", (VALUE(*)(...))new_method, -2);
+}
+
+#endif // HAVE_LIBINT2_H
--- ext/ruphy_libint2/evaluator.h
+++ ext/ruphy_libint2/evaluator.h
@@ -1,2 +1,13 @@
 #pragma once
+typedef struct _evaluator_struct{
+	Libint_eri_t erieval;
+	LIBINT2_REALTYPE * buf;
+}evaluator_struct;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
 void ruphy_libint2_define_evaluator(VALUE libint2_module);
+#ifdef __cplusplus
+}
+#endif
