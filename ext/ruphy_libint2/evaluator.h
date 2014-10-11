f0e346a2184a35338025716840e7ad47f40c87fc
Katsuhiko Nishimra <ktns.87@gmail.com>
2014-10-11 12:52:32 +0900

    Indentation

--- ext/ruphy_libint2/evaluator.h
+++ ext/ruphy_libint2/evaluator.h
@@ -1,7 +1,7 @@
 #pragma once
 typedef struct _evaluator_struct{
-	Libint_eri_t *erieval;
-	LIBINT2_REALTYPE * buf;
+  Libint_eri_t *erieval;
+  LIBINT2_REALTYPE * buf;
 }evaluator_struct;
 
 #ifdef __cplusplus
@@ -11,3 +11,4 @@ void ruphy_libint2_define_evaluator(VALUE libint2_module);
 #ifdef __cplusplus
 }
 #endif
+/* vim: set et sw=2 : */
--- ext/ruphy_libint2/ruphy_libint2.cc
+++ ext/ruphy_libint2/ruphy_libint2.cc
@@ -6,32 +6,33 @@
 #include "evaluator.h"
 
 extern "C" {
-	static VALUE return_true(){return Qtrue;};
-	static VALUE return_false(){return Qfalse;};
+  static VALUE return_true(){return Qtrue;};
+  static VALUE return_false(){return Qfalse;};
 
-	static void static_cleanup(VALUE p){
-		LIBINT2_PREFIXED_NAME(libint2_static_cleanup)();
-	}
+  static void static_cleanup(VALUE p){
+    LIBINT2_PREFIXED_NAME(libint2_static_cleanup)();
+  }
 }
 
 static VALUE RuPHY;
 static VALUE Libint2;
 
 extern "C" void Init_ruphy_libint2(){
-	RuPHY=rb_define_module("RuPHY");
-	Libint2=rb_define_module_under(RuPHY,"Libint2");
+  RuPHY=rb_define_module("RuPHY");
+  Libint2=rb_define_module_under(RuPHY,"Libint2");
 #ifdef HAVE_LIBINT2_INIT_ERI
-	rb_define_singleton_method(Libint2, "compiled?", RUBY_METHOD_FUNC(return_true),  0);
+  rb_define_singleton_method(Libint2, "compiled?", RUBY_METHOD_FUNC(return_true),  0);
 #ifdef RUPHY_ENDPROC_VOID
-	rb_set_end_proc((void*)static_cleanup, Qnil);
+  rb_set_end_proc((void*)static_cleanup, Qnil);
 #elif defined(RUPHY_ENDPROC_FUNC)
-	rb_set_end_proc((void(*)(VALUE))static_cleanup, Qnil);
+  rb_set_end_proc((void(*)(VALUE))static_cleanup, Qnil);
 #else
-	rb_set_end_proc(static_cleanup, Qnil);
+  rb_set_end_proc(static_cleanup, Qnil);
 #endif
-	LIBINT2_PREFIXED_NAME(libint2_static_init)();
-	ruphy_libint2_define_evaluator(Libint2);
+  LIBINT2_PREFIXED_NAME(libint2_static_init)();
+  ruphy_libint2_define_evaluator(Libint2);
 #else
-	rb_define_singleton_method(Libint2, "compiled?", RUBY_METHOD_FUNC(return_false), 0);
+  rb_define_singleton_method(Libint2, "compiled?", RUBY_METHOD_FUNC(return_false), 0);
 #endif
 }
+/* vim: set et sw=2 : */
