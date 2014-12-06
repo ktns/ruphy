5f322d2b64a6b36e4349629f63d89b1ff4a811f8
Katsuhiko Nishimra <ktns.87@gmail.com>
2014-12-07 04:35:41 +0900

    Workaround for rubinius
    [ci skip]

--- prof/ruphy_prof.rb
+++ prof/ruphy_prof.rb
@@ -2,7 +2,9 @@ $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)+'/../lib'))
 require 'ruphy'
 require 'ruphy/basisset/STO3G'
 
-RubyVM::InstructionSequence.compile_option = {
-  :tailcall_optimization => true,
-  :trace_instruction => false
-}
+if defined? RubyVM::InstructionSequence
+  RubyVM::InstructionSequence.compile_option = {
+    :tailcall_optimization => true,
+    :trace_instruction => false
+  }
+end
